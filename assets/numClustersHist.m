7878355ad9356eaf70adc4b127f963326ef9dad8
Xinlong Du <du.xinl@husky.neu.edu>
2022-06-12 19:27:48 -0400

    Save the histogram for the number of clusters

--- assets/numClustersHist.m
+++ assets/numClustersHist.m
@@ -1,6 +1,18 @@
 clear;clc;
 load('numClusters.txt');
-histogram(numClusters)
+hfig=figure;
+histogram(numClusters,'FaceColor','none')
 set(gca,'XTick',(4:1:8))
-xlabel('Number of clusters')
-ylabel('Number of grids')
+set(gca,'FontSize',8,'FontName','Times New Roman')
+xlabel('Number of clusters','FontSize',8,'FontName','Times New Roman')
+ylabel('Number of grids','FontSize',8,'FontName','Times New Roman')
+
+% save histogram
+figWidth=3.5;
+figHeight=3;
+set(hfig,'PaperUnits','inches');
+set(hfig,'PaperPosition',[0 0 figWidth figHeight]);
+figname=('.\Fig23.'); %Fig. 23 in the paper
+print(hfig,[figname,'jpg'],'-r1000','-djpeg');
+
+meanNumClusters=mean(numClusters);
